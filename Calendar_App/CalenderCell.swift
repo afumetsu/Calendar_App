7154c7f6b33cf5914d9644e9dcc490479bc77faa
ahmetozcan <ametoneamet@gmail.com>
2023-06-29 17:37:35 +0300

    Initial Commit

---
+++ Calendar_App/CalenderCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalenderCell.swift
+//  Calendar_App
+//
+//  Created by Math on 29.06.2023.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
